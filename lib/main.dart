0051aad854bb62d590f160a0eafeaa4f3e5788af
codify <hosseinmaleki.dev@gmail.com>
2024-09-14 02:37:21 +0330

    splashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -90,19 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+      
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
